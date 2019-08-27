065c770aaf4bbc828c4bedfc7e079dc8977ea760
guiemi <guiemi.teixeira@gmail.com>
2019-08-27 19:16:46 -0300

    Seta a view inicial e reseta o CSS inicial

--- bissexto/settings.py
+++ bissexto/settings.py
@@ -120,4 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = (
+    os.path.dirname(os.path.abspath(__file__))
+)
+
 STATIC_URL = '/static/'
