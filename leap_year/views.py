d093befb8c60e6936f4551e841c794d908f1f73e
guiemi <guiemi.teixeira@gmail.com>
2019-08-27 21:42:23 -0300

    formatação do código

--- leap_year/views.py
+++ leap_year/views.py
@@ -5,6 +5,9 @@ from leap_year.models import LeapYear
 def index(request):
     years = LeapYear.objects.order_by("id")
 
-    context = {"years": years}
-    return render(request, "leap_year/index.html", context)
+    context = {
+        'years': years,
+    }
+    return render(request, 'leap_year/index.html', context)
+
 
